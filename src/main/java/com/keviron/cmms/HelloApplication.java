7c2df14b2667b645317daa373f5c340f076bda97
Marcel <keviron4yt@gmail.com>
2022-05-23 21:36:04 +0200

    GUI - Base

--- src/main/java/com/keviron/cmms/HelloApplication.java
+++ src/main/java/com/keviron/cmms/HelloApplication.java
@@ -2,17 +2,26 @@ package com.keviron.cmms;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/Main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 650);
+
+        scene.getStylesheets().add(getClass().getResource("/css/Main.css").toExternalForm());
+        scene.setFill(Color.TRANSPARENT);
+
+        stage.setResizable(false);
+        stage.setTitle("CMMS - 2022 LTS!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/keviron/cmms/HelloController.java
+++ src/main/java/com/keviron/cmms/HelloController.java
@@ -1,14 +1,91 @@
 package com.keviron.cmms;
 
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+    private int am = 1;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private AnchorPane MenuPanel;
+
+    @FXML
+    private Button clientsButton;
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button repairsButton;
+
+    @FXML
+    private StackPane ContentPanel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)  {
+
+    }
+
+
+    @FXML
+    public void switchToRepairs(ActionEvent evt) throws IOException {
+        if (am == 2) {return;}
+        am = 2;
+
+        FadeTransition FadeOut = new FadeTransition(Duration.millis(500), ContentPanel);
+        makeFadeTransition(FadeOut, 1.0, 0.0);
+
+        FadeOut.setOnFinished((e) -> {
+            try {
+                ContentPanel.getChildren().clear();
+                ContentPanel.getChildren().add(FXMLLoader.load(getClass().getResource("/fxml/Repairs.fxml")));
+
+                FadeTransition FadeIn = new FadeTransition(Duration.millis(500), ContentPanel);
+                makeFadeTransition(FadeIn, 0.0, 1.0);
+
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+    }
+
+    @FXML
+    public void switchToClients(ActionEvent evt) throws IOException {
+        if (am == 2) {return;}
+        am = 2;
+
+        FadeTransition FadeOut = new FadeTransition(Duration.millis(500), ContentPanel);
+        makeFadeTransition(FadeOut, 1.0, 0.0);
+
+        FadeOut.setOnFinished((e) -> {
+            try {
+                ContentPanel.getChildren().clear();
+                ContentPanel.getChildren().add(FXMLLoader.load(getClass().getResource("/fxml/Clients.fxml")));
+
+                FadeTransition FadeIn = new FadeTransition(Duration.millis(500), ContentPanel);
+                makeFadeTransition(FadeIn, 0.0, 1.0);
+
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+    }
+
+    private void makeFadeTransition(FadeTransition fade, double StartValue , double EndValue){
+        fade.setFromValue(StartValue);
+        fade.setToValue(EndValue);
+        fade.play();
     }
 }
